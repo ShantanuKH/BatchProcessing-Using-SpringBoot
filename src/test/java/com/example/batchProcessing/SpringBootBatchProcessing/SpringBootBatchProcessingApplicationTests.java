c0fb6078502d708bf1667372a11d6abc72451874
ShantanuKH <khadseshantanu02@gmail.com>
2025-11-18 19:39:29 +0530

    Started Spring Boot Batch Processing Project

---
+++ src/test/java/com/example/batchProcessing/SpringBootBatchProcessing/SpringBootBatchProcessingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.batchProcessing.SpringBootBatchProcessing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBatchProcessingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
